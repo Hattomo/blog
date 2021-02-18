8be0043d77609463c7b087c2b9f8f4ed2f8e917a
Hattomo <45988993+Hattomo@users.noreply.github.com>
2021-02-18 11:35:15 +0900

    Update new.sh

--- new.sh
+++ new.sh
@@ -3,7 +3,17 @@
 
 title=$1
 year=`date '+%y'`
-quoter=`date '+Q%q'`
-date=`date '+%m%d-'`
-path=posts/$year/$quoter/$date$title/index.md
+month=`date '+%m'`
+date=`date '+%d-'`
+#quoter=`date '+Q%q'` # for Linux, not for macOS
+if [ $month == 01 ] || [ $month == 02 ] || [ $month == 03 ]; then
+  quoter=1
+elif [ $month == 04 ] || [ $month == 05 ] || [ $month == 06 ]; then
+  quoter=2
+elif [ $month == 07 ] || [ $month == 08 ] || [ $month == 09 ]; then
+    quoter=3
+else
+    quoter=4
+fi
+path=posts/$year/Q$quoter/$month$date$title/index.md
 hugo new $path
